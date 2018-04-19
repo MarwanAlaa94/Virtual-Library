90b3ac885dc14e7a41ae27f79cda264383ea0438
MarwanAlaa94 <sheetoss94@gmail.com>
2018-04-19 16:53:21 +0200

    get the test environment working

---
+++ src/test/java/com/virtualLibrary/retreive/BookBrowserTest.java
@@ -0,0 +1,14 @@
+package com.virtualLibrary.retreive;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookBrowserTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
