45761c900e71a9ac86e8e8042e5b3f792c95ae52
fathy <muhammed.fat7y@gmail.com>
2018-04-19 21:50:57 +0200

    Add test case to BookBrowser Test

--- src/test/java/com/virtualLibrary/retreive/BookBrowserTest.java
+++ src/test/java/com/virtualLibrary/retreive/BookBrowserTest.java
@@ -1,12 +1,18 @@
 package com.virtualLibrary.retreive;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -14,6 +20,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.ui.ModelMap;
 
 import com.google.api.services.books.Books;
+import com.virtualLibrary.model.Book;
 import com.virtualLibrary.model.BrowsingModel;
 
 public class BookBrowserTest {
@@ -36,5 +43,19 @@ public class BookBrowserTest {
 		String drama = "Drama";
 		browser.getBookGrid(modelMap, drama);
 		verify(model).search(any(Books.class), anyString(), anyString(), Mockito.anyLong());
+	}
+	
+	@Test
+	public void testGetBookGridParameters() {
+		String drama = "Drama";
+		ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
+		
+		browser.getBookGrid(modelMap, drama);
+		
+		verify(model).search(any(Books.class), stringCaptor.capture(), stringCaptor.capture(), Mockito.anyLong());
+		java.util.List<String> capturedStrings = stringCaptor.getAllValues();
+		
+		assertEquals("category", capturedStrings.get(0));
+		assertEquals(drama, capturedStrings.get(1));
 	}
 }
